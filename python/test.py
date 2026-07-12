afd8b77cc4dbd902531c36d121cbd3f104dfa933
ironcristian <ironcristian14380@gmail.com>
2026-07-12 01:54:53 +0100

    Update test.py

--- python/test.py
+++ python/test.py
@@ -10,10 +10,10 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 screen_width, screen_height = pyautogui.size()
 
 screen = {
-    "left": int((1327 / 1920) * screen_width),
-    "top": int((1050 / 1080) * screen_height),
-    "width": int(((1406 - 1327) / 1920) * screen_width),
-    "height": int(((1078 - 1050) / 1080) * screen_height)
+    "left": int((1273 / 1920) * screen_width),
+    "top": int((778 / 1080) * screen_height),
+    "width": int(((1311 - 1273) / 1920) * screen_width),
+    "height": int(((816 - 778) / 1080) * screen_height)
 }
 
 
@@ -28,22 +28,7 @@ with mss.MSS() as sct:
     )
 
     # Save original
-    image.save("original.png")
-
-    # Scale
-    image = image.resize(
-        (image.width * 4, image.height * 4),
-        Image.Resampling.NEAREST
-    )
-
-    image.save("scaled.png")
+    image.save("../Auto_Rebirther_Screenshots/X_Rebirth_Screenshot_Permanent/x_symbol.png")
 
 
-    # Test different OCR modes
-    for mode in [6, 7, 11, 13]:
-        text = pytesseract.image_to_string(
-            image,
-            config=f"--psm {mode}"
-        )
-
-        print(f"PSM {mode}: {repr(text)}")
+  
