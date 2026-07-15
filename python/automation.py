c1dfd1e1f69f963896311ce8b536f8015cdd49d0
ironcristian <ironcristian14380@gmail.com>
2026-07-15 02:16:48 +0100

    Added to new function.
    Added a portal_page_check function that checks what page I'm currently on inside the travel portal page. It returns the page difference. It can either be negative or positive. If its negative it means you have to go "Back", if positive, it means "Next". Also added a portal_is_locked function. Upon reaching the correct page where the zone you have chosen is located it will check if that zone is locked for you. If it  is it will let you know and close the portal page. If not it will proceed with thee auto-run.

--- python/automation.py
+++ python/automation.py
@@ -11,6 +11,8 @@ import win32con
 import configparser
 import subprocess
 import cv2
+import sys
+from pathlib import Path
 
 import constants
 
@@ -36,17 +38,21 @@ def start_player_setup():
 
 def start_ahk_script():
     global ahk_process
-
+    
+    print("Starting ahk script")
     ahk_process = subprocess.Popen(
-    [
-        r"C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe",
-        "../ahk/main.ahk"
-    ]
-)
+        [
+            r"C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe",
+            "../ahk/main.ahk"
+        ]
+    )
+
+    print("Started ahk script.")
 
 
 def focus_roblox_window():
 
+    print("Putting Roblox window into focus.")
     window_title = "Roblox"
     hwnd = win32gui.FindWindow(None, window_title) # The none argument specified the classname. We dont care about that. Also hwnd means h=handle, wnd=window. {Handle to a Window}
 
@@ -74,7 +80,7 @@ def find_player_portal_button():
 
     focus_roblox_window()
 
-    template = cv2.imread("../Auto_Rebirther_Screenshots/Portal_Button_Screenshot_Permanent/portal_button.png")
+    template = cv2.imread("../screenshots/portal_button.png")
 
     with mss.MSS() as sct:
 
@@ -115,8 +121,10 @@ def find_player_portal_button():
             # Imagine this. Forget about the coords of the pic. Say we find the portal at  1050. And the portal spans from 1050 down to 1060. Now lets say to make cv2 do less work we first crop the area where,
             # We know the image will be. We know it will be at the bottom somewhere anywhere along the x axis but always after y = 1040. Now cropped region will be 1920, 40. When we run cv2 on this image,
             # It will return something like 1500, 10. Because top of image starts at y = 0. Now to get the real coordinates back we need to add 10 + 1040. So now we get the real screen coordinates
- 
+            print(f"Located portal travel button at: {real_x}, {real_y}")
             save_portal_coordinates(real_x / 1920, real_y / 1080)
+            print("Saved button coordinates inside ini file.")
+
         else:
             print("Travel button not found. Do you have Fast Travel activated in the game settings?")
 
@@ -127,7 +135,7 @@ def find_player_portal_button():
 # Image Comparison
 # ==========================
 
-def image_similarity(image1, image2, threshold=10):
+def image_similarity(image1, image2, threshold=2):
 
     difference = ImageChops.difference(image1, image2)
 
@@ -135,7 +143,7 @@ def image_similarity(image1, image2, threshold=10):
 
     average_pixel_difference = difference_np_array.mean()
 
-    print(average_pixel_difference)
+    print(average_pixel_difference, image1, image2)
 
     return average_pixel_difference < threshold
 
@@ -143,6 +151,102 @@ def image_similarity(image1, image2, threshold=10):
 # ==========================
 # Screenshot Analysis
 # ==========================
+
+def portal_page_check(zone):
+
+
+    next_counter = 0 # Amount of "Next" buttons we need to press to get to out desired page
+    folders = [
+        Path("../screenshots/portal_screenshots"),
+        Path("../screenshots/portal_locked_screenshots")
+    ]
+
+    zone_string = zone.lower().replace(" ", "_") # Make text lower and replace white space with _. So "Green Hill" becomes "green_hill"
+
+    # This code is to check what page we are currently on. So we just cehck the first location of every page. {The top portal}
+    for folder in folders:
+        for file in folder.iterdir(): # Creates iterable containing all files in that folder
+
+            portal_name = file.name.replace("_portal.png", "").upper() # Removed "_portal" from filename and makes it capitalized so I can access the constants.
+            command_name = portal_name.replace("_", " ").title() # This basically converts "green_hill" into "Green Hill"
+            zone_portal_coordinates = constants.PORTAL_COORDINATES[portal_name]
+
+            with mss.MSS() as sct:
+
+                screen = {
+                    "left": int(zone_portal_coordinates["left"] * screen_width),
+                    "top": int(zone_portal_coordinates["top"] * screen_height),
+                    "width": int(zone_portal_coordinates["width"] * screen_width),
+                    "height": int(zone_portal_coordinates["height"] * screen_height)
+                }
+
+                screenshot = sct.grab(screen)
+
+                image = Image.frombytes(
+                    "RGB",
+                    screenshot.size,
+                    screenshot.rgb
+                )
+
+                page_distance = constants.ZONES_PAGE_NUMBER[zone] - constants.ZONES_PAGE_NUMBER[command_name] 
+
+
+                if image_similarity(portal_image, image):
+
+                    if "locked" not in file.name:
+                        return page_distance
+
+                    if zone == command_name:
+                        print(f"{command_name} is currently locked.")
+                        return None
+
+                    return page_distance
+                    
+   
+    
+
+
+
+
+def portal_is_locked(zone):
+
+
+    file_name = f"{zone.replace(' ', '_').lower()}_locked_portal.png"
+    with mss.MSS() as sct:
+
+        screen = {
+            "left": int(constants.ZONE_BUTTON_COORDINATES[zone]["left"] * screen_height),
+            "top": int(constants.ZONE_BUTTON_COORDINATES[zone]["top"] * screen_height),
+            "width": int(constants.ZONE_BUTTON_COORDINATES[zone]["width"] * screen_height),
+            "height": int(constants.ZONE_BUTTON_COORDINATES[zone]["height"] * screen_height)
+        } 
+
+        screenshot = sct.grab(screen)
+
+        image = Image.frombytes(
+            "RGB",
+            screenshot.size,
+            screenshot.rgb
+        )
+
+        portal_screenshot = Image.open(f"../screenshots/portal_locked_screenshots/{file_name}")
+        if image_similarity(image, portal_screenshot):
+            print(f"{zone} is currently locked. Please choose another zone.")
+            return True
+        
+
+        return False
+
+
+
+
+
+
+
+
+
+
+
 
 def take_screenshot_and_analyze_level():
 
@@ -164,10 +268,11 @@ def take_screenshot_and_analyze_level():
         )
 
         level_screenshot = Image.open(
-            "../Auto_Rebirther_Screenshots/Level_Bar_Permanent/level_bar_section.png"
+            "../screenshots/level_bar_section.png"
         )
 
         if image_similarity(image, level_screenshot):
+            print("Detected max level. Rebirth available")
             return True
         else:
             return False
@@ -193,17 +298,18 @@ def take_screenshot_and_analyze_egg():
         )
 
         egg_screenshot = Image.open(
-            "../Auto_Rebirther_Screenshots/Egg_Notification_Screenshot_Permanent/egg_notification.png"
+            "../screenshots/egg_notification.png"
         )
 
         if list(image.getdata()) == list(egg_screenshot.getdata()):
+            print("Detected egg notification")
             return True
         else:
             return False
 
 
 def auto_run_entered_check(zone):
-    time.sleep(3)
+    time.sleep(2) # This is to wait for the black screen animation to pass when you enter auto-run
     with mss.MSS() as sct:
         screen = {
             "left": int((1273 / 1920) * screen_width),
@@ -220,9 +326,10 @@ def auto_run_entered_check(zone):
             screenshot.rgb # This is the actual raw pixel data. Example: (205, 54, 23)
         )
 
-        x_screenshot = Image.open("../Auto_Rebirther_Screenshots/X_Rebirth_Screenshot_Permanent/x_symbol.png")
+        x_screenshot = Image.open("../screenshots/x_symbol.png")
 
         if not image_similarity(image, x_screenshot):
+            print(f"Auto-run zone has not succesfully been entered. Sending the command {zone} again.")
             send_command(zone)
 
 # ==========================
@@ -230,18 +337,50 @@ def auto_run_entered_check(zone):
 # ==========================
 
 def send_command(command):
+    global rebirthing_in_progress
+    page_distance = None
+
+
+    if command in constants.ZONES:
+        send_command("Click_Portal") 
+        # This will send a command to click the portal button. The code will be stuck on this until it recieved a done.txt.
+        # I will then detect what page we are on and change to the right one if the player is not on it. And then I will check if the player has that place unlocked.
+
+        distance = portal_page_check(command)
+
+        if distance is None:
+            send_command("Close_Portal")
+            return 
+        page_distance = distance
+
+    
 
     with open("../command.txt", "w") as f:
+        print(f"Sending command: {command}")
         f.write(f"{command}")
+        
+        if page_distance is not None and page_distance != 0: # We check if its non None so in case page_distance is 0 
+            f.write(f"\n{page_distance}")
 
+
+
+    print(f"Checking if ahk has finished performing command: {command}")
     while not os.path.exists("../done.txt"):
         time.sleep(0.1)
 
+    print(f"ahk has finished performing: {command}")
+
     os.remove("../done.txt")
+    print("Done file deleted:", not os.path.exists("../done.txt"))
+
+    if command == "Rebirth":
+        print("Rebirth no longer in progress")
+        rebirthing_in_progress = False
 
 
 def clear_command_and_close_ahk():
-
+    
+    print("Clearing command.txt file.")
     with open("../command.txt", "w") as f:
         pass
 
@@ -258,6 +397,8 @@ last_xp_claim = time.time()
 last_image_analysis = time.time()
 last_egg_check = time.time()
 
+rebirthing_in_progress = False
+
 
 # ==========================
 # Main Automation Loop
@@ -265,14 +406,13 @@ last_egg_check = time.time()
 
 def begin_automation(zone):
     
-    global last_level_change, last_xp_claim, last_image_analysis, last_egg_check
+    global last_level_change, last_xp_claim, last_image_analysis, last_egg_check, rebirthing_in_progress
+
+    if rebirthing_in_progress:
+        rebirthing_in_progress = False
 
     start_player_setup()
-    print("Starting ahk script.")
     start_ahk_script()
-    print("Started ahk script.")
-
-    print("Starting automation.")
 
     print(f"Setting up {zone}")
     send_command(zone)
@@ -291,6 +431,7 @@ def begin_automation(zone):
 
                 print("Claiming Egg")
                 send_command("Claim_EGG")
+                print("Checking if auto-run zone has been succesfully entered.")
                 auto_run_entered_check(zone)
                 print("Claimed Egg")
 
@@ -309,10 +450,13 @@ def begin_automation(zone):
 
 
             else:
-                if current_time - last_level_change > 30:
+                if current_time - last_level_change > 30 and not rebirthing_in_progress:
 
-                    print("Rebirthing")
-                    send_command("Rebirth")
+                    print("Starting Rebirth")
+                    send_command("Rebirth") # We check if we entered the auto-run area becuase at the end of Rebirth we walk back to auto-run area.
+                    rebirthing_in_progress = True
+                    print("Rebirth in progress")
+                    print("Checking if auto-run zone has been succesfully entered.")
                     auto_run_entered_check(zone)
                     print("Rebirthed")
 
@@ -329,6 +473,8 @@ def begin_automation(zone):
 
             print("Claiming XP")
             send_command("Claim_XP")
+            print("Checking if auto-run zone has been succesfully entered.")
+            auto_run_entered_check(zone)
             print("Claimed XP")
 
             last_xp_claim = current_time
