c41e1f39ff47afa33c36925ac6a875a37bcd6dd4
ironcristian <ironcristian14380@gmail.com>
2026-07-15 02:18:18 +0100

    Updated some constants. Still need to fix the ZONE_BUTTON_COORDINATES.
    Also added  ZONE_PAGE_NUMBER constant which tells me the page number of each zone. This is what I use to calculate the page difference inside automation.py.

--- python/constants.py
+++ python/constants.py
@@ -24,61 +24,142 @@ POSSIBLE_PORTAL_LOCATION_WIDTH_RATIO = 1920 / 1920
 POSSIBLE_PORTAL_LOCATION_HEIGHT_RATIO = 990 / 1080
 
 
-POSSIBLE_PORTAL_COORDINATES = [
-    {
-        "name": "portal_0",
-        "left_ratio": 1054 / 1920,
-        "top_ratio": 1049 / 1080,
-        "width_ratio": (1132 - 1054) / 1920,
-        "height_ratio": (1078 - 1049) / 1080,
-        "button_coordinates": (1097, 1036)
+ZONE_BUTTON_COORDINATES = {
+    "LOST_VALLEY": {
+        "left": 1192,
+        "top": 378,
+        "width": 1240 - 1192,  # 48
+        "height": 420 - 378    # 42
     },
-    {
-        "name": "portal_1",
-        "left_ratio": 1101 / 1920,
-        "top_ratio": 1053 / 1080,
-        "width_ratio": (1178 - 1101) / 1920,
-        "height_ratio": (1079 - 1053) / 1080,
-        "button_coordinates": (1140, 1033)
+    "EMERALD_HILL": {
+        "left": 1098,
+        "top": 691,
+        "width": 1142 - 1098,  # 44
+        "height": 728 - 691    # 37
     },
-    {
-        "name": "portal_2",
-        "left_ratio": 1147 / 1920,
-        "top_ratio": 1054 / 1080,
-        "width_ratio": (1222 - 1147) / 1920,
-        "height_ratio": (1079 - 1054) / 1080,
-        "button_coordinates": (1095, 1033)
+    "HILL_TOP": {
+        "left": 678,
+        "top": 385,
+        "width": 726 - 678,    # 48
+        "height": 421 - 385    # 36
     },
-    {
-        "name": "portal_3",
-        "left_ratio": 1192 / 1920,
-        "top_ratio": 1054 / 1080,
-        "width_ratio": (1269 - 1192) / 1920,
-        "height_ratio": (1079 - 1054) / 1080,
-        "button_coordinates": (1231, 1031)
+
+    "SPEED_JUNGLE": {
+        "left": 934,
+        "top": 176,
+        "width": 990 - 934,    # 56
+        "height": 236 - 176    # 60
     },
-    {
-        "name": "portal_4",
-        "left_ratio": 1235 / 1920,
-        "top_ratio": 1051 / 1080,
-        "width_ratio": (1315 - 1235) / 1920,
-        "height_ratio": (1078 - 1051) / 1080,
-        "button_coordinates": (1278, 1033)
+    "NO_PLACE": {
+        "left": 1190,
+        "top": 360,
+        "width": 1247 - 1190,  # 57
+        "height": 422 - 360    # 62
     },
-    {
-        "name": "portal_5",
-        "left_ratio": 1283 / 1920,
-        "top_ratio": 1052 / 1080,
-        "width_ratio": (1360 - 1283) / 1920,
-        "height_ratio": (1079 - 1052) / 1080,
-        "button_coordinates": (1321, 1035)
+    "CYBER_STATION": {
+        "left": 1091,
+        "top": 665,
+        "width": 1148 - 1091,  # 57
+        "height": 724 - 665    # 59
     },
-    {
-        "name": "portal_5",
-        "left_ratio": 1327 / 1920,
-        "top_ratio": 1050 / 1080,
-        "width_ratio": (1406 - 1327) / 1920,
-        "height_ratio": (1078 - 1050) / 1080,
-        "button_coordinates": (1367, 1035)
+    "NEW_YOKE": {
+        "left": 673,
+        "top": 361,
+        "width": 731 - 673,    # 58
+        "height": 425 - 361    # 64
+    },
+
+    "METRO_CITY": {
+        "left": 675,
+        "top": 363,
+        "width": 732 - 675,    # 57
+        "height": 423 - 363    # 60
     }
-]
+}
+
+
+
+ZONES_PAGE_NUMBER = {
+    "Green Hill": 0,
+    "Lost City": 0,
+    "Emerald Hill": 0,
+    "City Escape": 0,
+    "Hill Top": 0,
+
+    "Speed Jungle": 1,
+    "No Place": 1,
+    "Cyber Station": 1,
+    "Metal City": 1,
+    "New Yoke": 1,
+
+    "Space Colony": 2,
+    "Radical Highway": 2,
+    "Pupmkin HilL": 2,
+    "Autumn Forest": 2,
+    "Metro City": 2,
+
+    "Cool Edge": 3,
+    "Kronos Island": 3,
+    "Pixel Green": 3
+
+}
+
+
+# POSSIBLE_PORTAL_COORDINATES = [
+#     {
+#         "name": "portal_0",
+#         "left_ratio": 1054 / 1920,
+#         "top_ratio": 1049 / 1080,
+#         "width_ratio": (1132 - 1054) / 1920,
+#         "height_ratio": (1078 - 1049) / 1080,
+#         "button_coordinates": (1097, 1036)
+#     },
+#     {
+#         "name": "portal_1",
+#         "left_ratio": 1101 / 1920,
+#         "top_ratio": 1053 / 1080,
+#         "width_ratio": (1178 - 1101) / 1920,
+#         "height_ratio": (1079 - 1053) / 1080,
+#         "button_coordinates": (1140, 1033)
+#     },
+#     {
+#         "name": "portal_2",
+#         "left_ratio": 1147 / 1920,
+#         "top_ratio": 1054 / 1080,
+#         "width_ratio": (1222 - 1147) / 1920,
+#         "height_ratio": (1079 - 1054) / 1080,
+#         "button_coordinates": (1095, 1033)
+#     },
+#     {
+#         "name": "portal_3",
+#         "left_ratio": 1192 / 1920,
+#         "top_ratio": 1054 / 1080,
+#         "width_ratio": (1269 - 1192) / 1920,
+#         "height_ratio": (1079 - 1054) / 1080,
+#         "button_coordinates": (1231, 1031)
+#     },
+#     {
+#         "name": "portal_4",
+#         "left_ratio": 1235 / 1920,
+#         "top_ratio": 1051 / 1080,
+#         "width_ratio": (1315 - 1235) / 1920,
+#         "height_ratio": (1078 - 1051) / 1080,
+#         "button_coordinates": (1278, 1033)
+#     },
+#     {
+#         "name": "portal_5",
+#         "left_ratio": 1283 / 1920,
+#         "top_ratio": 1052 / 1080,
+#         "width_ratio": (1360 - 1283) / 1920,
+#         "height_ratio": (1079 - 1052) / 1080,
+#         "button_coordinates": (1321, 1035)
+#     },
+#     {
+#         "name": "portal_5",
+#         "left_ratio": 1327 / 1920,
+#         "top_ratio": 1050 / 1080,
+#         "width_ratio": (1406 - 1327) / 1920,
+#         "height_ratio": (1078 - 1050) / 1080,
+#         "button_coordinates": (1367, 1035)
+#     }
+# ]
