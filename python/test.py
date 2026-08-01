ba691b5748c021707d95dd47a352d14bfd7e244c
ironcristian <ironcristian14380@gmail.com>
2026-08-02 00:31:38 +0100

    Test

--- python/test.py
+++ python/test.py
@@ -1,12 +1,15 @@
 import mss
+import pyautogui
 from PIL import Image
 
+screen_width, screen_height = pyautogui.size()
+
 ZONE_BUTTON_COORDINATES = {
-    "COOL EDGE": {
-        "left": 934,
-        "top": 176,
-        "width": 990 - 934,    # 56
-        "height": 236 - 176    # 60
+    "Cool Edge": {
+        "left": 934 / 1920,
+        "top": 176 / 1080,
+        "width": (990 - 934) / 1920,
+        "height": (236 - 176) / 1080
     }
 }
 
@@ -14,10 +17,10 @@ with mss.MSS() as sct:
     for zone, coords in ZONE_BUTTON_COORDINATES.items():
 
         screen = {
-            "left": coords["left"],
-            "top": coords["top"],
-            "width": coords["width"],
-            "height": coords["height"]
+            "left": int(coords["left"] * screen_width),
+            "top": int(coords["top"] * screen_height),
+            "width": int(coords["width"] * screen_width),
+            "height": int(coords["height"] * screen_height)
         }
 
         screenshot = sct.grab(screen)
@@ -28,7 +31,7 @@ with mss.MSS() as sct:
             screenshot.rgb
         )
 
-        filename = f"{zone.replace(' ', '_').lower()}_locked_portal.png"
+        filename = f"{zone.lower().replace(' ', '_')}_portal.png"
         image.save(filename)
 
         print(f"Saved {filename}")
