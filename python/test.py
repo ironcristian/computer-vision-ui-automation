00a990f3d5bcc4a17254a098a0a1413e05a3ec49
ironcristian <ironcristian14380@gmail.com>
2026-08-10 23:08:45 +0100

    Just come image similarity testing between 2 devices with same resolution

--- python/test.py
+++ python/test.py
@@ -1,4 +1,5 @@
 import cv2
+<<<<<<< Updated upstream
 
 image1 = cv2.imread("cool_edge_portalsssssss.png")
 image2 = cv2.imread("cool_edge_portals1.png")
@@ -25,3 +26,51 @@ diff = cv2.absdiff(image1, image2)
 cv2.imwrite("cool_edge_difference.png", diff)
 
 print("Saved cool_edge_difference.png")
+=======
+import mss
+import numpy as np
+from PIL import Image
+
+with mss.MSS() as sct:
+
+    # Original 1920x1080 coordinates
+    screen = {
+        "left": 675,
+        "top": 363,
+        "width": 732 - 675,   # 57
+        "height": 423 - 363   # 60
+    }
+
+    screenshot = sct.grab(screen)
+
+    image = Image.frombytes(
+        "RGB",
+        screenshot.size,
+        screenshot.rgb
+    )
+
+    image.save("metro_city_1920.png")
+    print("Saved metro_city_1920.png")
+
+# Load both images
+template = cv2.imread("../screenshots/portal_screenshots/metro_city_portal.png")
+current = cv2.imread("metro_city_1920.png")
+
+# Compare
+result = cv2.matchTemplate(
+    current,
+    template,
+    cv2.TM_CCORR_NORMED
+)
+
+_, confidence, _, location = cv2.minMaxLoc(result)
+
+print(f"Similarity: {confidence:.6f}")
+print(f"Best location: {location}")
+
+# Save difference image
+diff = cv2.absdiff(template, current)
+cv2.imwrite("difference_1920.png", diff)
+
+print("Saved difference_1920.png")
+>>>>>>> Stashed changes
