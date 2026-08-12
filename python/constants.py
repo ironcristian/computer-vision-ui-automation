93910347e41aae829355818da31fa365feec727a
ironcristian <ironcristian14380@gmail.com>
2026-08-12 12:42:50 +0100

    Update constants.py
    Added animation check pixel constant. THis will be the pixel i check for the black screen animation. Might change it to the very corner though.
    
    Added zones in zone_button_coordinates
    
    Fixed spelling mistake for Pumpkin Hill

--- python/constants.py
+++ python/constants.py
@@ -9,10 +9,6 @@ EGG_TOP_RATIO= 574 / 1080
 EGG_WIDTH_RATIO = (314 - 63) / 1920
 EGG_HEIGHT_RATIO = (623 - 574) / 1080
 
-
-1305, 1024
-1338, 1054
-
 PORTAL_LEFT_RATIO = 1305 / 1920
 PORTAL_TOP_RATIO = 1024 / 1080
 PORTAL_WIDTH_RATIO = (1338 - 1305) / 1920
@@ -23,26 +19,43 @@ PORTAL_HEIGHT_RATIO = (1054 - 1024) / 1080
 POSSIBLE_PORTAL_LOCATION_WIDTH_RATIO = 1920 / 1920
 POSSIBLE_PORTAL_LOCATION_HEIGHT_RATIO = 990 / 1080
 
+ANIMATION_CHECK_PIXEL_WIDTH_RATIO = 75 / 1920
+ANIMATION_CHECK_PIXEL_HEIGHT_RATIO = 1045 / 1080
+
 
 ZONE_BUTTON_COORDINATES = {
+
+    "Green Hill": {
+        "left": 933 / 1920,
+        "top": 180 / 1080,
+        "width": (985 - 933) / 1920,
+        "height": (244 - 180) / 1080
+    },
     "Lost Valley": {
-        "left": 1192 / 1920,
-        "top": 378 / 1080,
-        "width": (1240 - 1192) / 1920,
-        "height": (420 - 378) / 1080
+        "left": 1190 / 1920,
+        "top": 353 / 1080,
+        "width": (1246 - 1190) / 1920,
+        "height": (428 - 353) / 1080
     },
     "Emerald Hill": {
-        "left": 1098 / 1920,
-        "top": 691 / 1080,
-        "width": (1142 - 1098) / 1920,
-        "height": (728 - 691) / 1080
+        "left": 1095 / 1920,
+        "top": 662 / 1080,
+        "width": (1149 - 1095) / 1920,
+        "height": (732 - 662) / 1080
     },
     "Hill Top": {
-        "left": 678 / 1920,
-        "top": 385 / 1080,
-        "width": (726 - 678) / 1920,
-        "height": (421 - 385) / 1080
+        "left": 680 / 1920,
+        "top": 357 / 1080,
+        "width": (733 - 680) / 1920,
+        "height": (428 - 357) / 1080
     },
+    "City Escape": {
+        "left": 777 / 1920,
+        "top": 668 / 1080,
+        "width": (828 - 777) / 1920,
+        "height": (729 - 668) / 1080
+    },
+    
 
     "Speed Jungle": {
         "left": 934 / 1920,
@@ -62,6 +75,12 @@ ZONE_BUTTON_COORDINATES = {
         "width": (1148 - 1091) / 1920,
         "height": (724 - 665) / 1080
     },
+    "Metal City": {
+        "left": 781 / 1920,
+        "top": 669 / 1080,
+        "width": (828 - 781) / 1920,
+        "height": (727 - 669) / 1080
+    },
     "New Yoke": {
         "left": 673 / 1920,
         "top": 361 / 1080,
@@ -69,6 +88,30 @@ ZONE_BUTTON_COORDINATES = {
         "height": (425 - 361) / 1080
     },
 
+    "Space Colony": {
+        "left": 940 / 1920,
+        "top": 182 / 1080,
+        "width": (984 - 940) / 1920,
+        "height": (238 - 182) / 1080
+    },
+    "Radical Highway": {
+        "left": 1195 / 1920,
+        "top": 369 / 1080,
+        "width": (1241 - 1195) / 1920,
+        "height": (424 - 369) / 1080
+    },
+    "Pumpkin Hill": {
+        "left": 1091 / 1920,
+        "top": 666 / 1080,
+        "width": (1151 - 1091) / 1920,
+        "height": (732 - 666) / 1080
+    },
+    "Autumn Forest": {
+        "left": 776 / 1920,
+        "top": 661 / 1080,
+        "width": (830 - 776) / 1920,
+        "height": (728 - 661) / 1080
+    },
     "Metro City": {
         "left": 675 / 1920,
         "top": 363 / 1080,
@@ -76,11 +119,24 @@ ZONE_BUTTON_COORDINATES = {
         "height": (423 - 363) / 1080
     },
 
+
     "Cool Edge": {
         "left": 934 / 1920,
         "top": 176 / 1080,
         "width": (990 - 934) / 1920,
         "height": (236 - 176) / 1080
+    },
+    "Kronos Island": {
+        "left": 1165 / 1920,
+        "top": 579 / 1080,
+        "width": (1225 - 1165) / 1920,
+        "height": (644 - 579) / 1080
+    },
+    "Pixel Green Hill": {
+        "left": 700 / 1920,
+        "top": 581 / 1080,
+        "width": (754 - 700) / 1920,
+        "height": (642 - 581) / 1080
     },
 }
 
@@ -101,7 +157,7 @@ ZONES_PAGE_NUMBER = {
 
     "Space Colony": 2,
     "Radical Highway": 2,
-    "Pupmkin HilL": 2,
+    "Pumpkin Hill": 2,
     "Autumn Forest": 2,
     "Metro City": 2,
 
